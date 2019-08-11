989dfbc9b9f6ac901330c3a363e8a2ab975c1d4b
CoolHandLuke88 <luke@LukesiMac-2.local>
2019-08-11 16:49:00 -0400

    add scripts for building docs

---
+++ Scripts/build_docs.sh
@@ -0,0 +1,27 @@
+
+#!/bin/bash
+
+# Docs by jazzy
+# https://github.com/realm/jazzy
+# ------------------------------
+
+if which jazzy >/dev/null; then
+    git submodule update --remote
+
+    jazzy \
+        --clean \
+        --min-acl internal \
+        --author 'Luke McDonald' \
+        --author_url 'https://github.com/devvman/client-ios-apa.git' \
+        --github_url 'https://github.com/devvman/client-ios-apa.git' \
+        --source-directory 'client-ios-apa/' \
+        --readme 'client-ios-apa/README.md' \
+        --output 'docs/'
+    exit
+else
+    echo "
+    Error: jazzy not installed! <https://github.com/realm/jazzy>
+    Install: gem install jazzy
+    "
+    exit 1
+fi
